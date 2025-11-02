e08177cb4f542136bfffd79659467e228aa0e612
Marcos F. Cantu <marcosfabiancantu@gmail.com>
2025-11-02 02:15:30 -0600

    Challenge 1 - Complete
    Added notes generated by ChatGPT

--- challenge-1/tf-challenge-1.tf
+++ challenge-1/tf-challenge-1.tf
@@ -1,17 +1,16 @@
 provider "aws" {
-  version = "~> 2.54"
-  region  = "us-east-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  region  = "us-east-1"
 }
-
-provider "digitalocean" {}
 
 terraform {
-    required_version = "0.12.31"
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
 }
 
-
 resource "aws_eip" "kplabs_app_ip" {
-  vpc      = true
+  domain = "vpc"
 }
